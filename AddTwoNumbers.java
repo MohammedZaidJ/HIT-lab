46bf06460bc07cbd5e794c930c4f84f0b0770d47
MohammedZaidJ <85501616+MohammedZaidJ@users.noreply.github.com>
2021-06-08 20:08:48 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,11 @@
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=20,n2=150,sum;
+		sum=n2+n1;
+		System.out.println("Sum.."+sum);
+	}
+
+}
